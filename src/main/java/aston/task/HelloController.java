18b8b5a3563e1b14f84c3fa0a20a0114b99a4ac3
noir1915 <astronaut95@list.ru>
2023-04-20 01:12:47 +0300

    init commit

---
+++ src/main/java/aston/task/HelloController.java
@@ -0,0 +1,14 @@
+package aston.task;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("say")
+public class HelloController {
+    @GetMapping(value = "hello")
+    public String sayHello() {
+        return "Hello world!";
+    }
+} 
