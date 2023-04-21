548a97d8d4fc474467a7fd5d9287cf3870f73a91
noir1915 <astronaut95@list.ru>
2023-04-21 14:57:21 +0300

    change pom

--- src/main/java/aston/task/HelloController.java
+++ src/main/java/aston/task/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping(value = "hello")
     public String sayHello() {
-        return "Hello world!";
+        return "2 ночи не спал, делал, получилось!!! е-маё";
     }
 } 
