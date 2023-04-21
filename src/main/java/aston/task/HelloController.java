502066f8c7d072c17cbf60262c5be9ad8f5140b3
noir1915 <astronaut95@list.ru>
2023-04-21 15:12:35 +0300

    change HelloController

--- src/main/java/aston/task/HelloController.java
+++ src/main/java/aston/task/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping(value = "hello")
     public String sayHello() {
-        return "Hello, World!";
+        return "Привет!";
     }
 }
